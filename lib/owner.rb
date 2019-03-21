34a27a61cce12f6e315528188862193937e95b5f
Cody Yantis <cyantis@gmail.com>
2019-03-21 18:58:22 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -34,17 +34,17 @@ class Owner
 
   def buy_fish(fish)
     new_fish = Fish.new(fish)
-    self.pets[:fishes] << new_fish.name
+    self.pets[:fishes] << new_fish
   end
 
   def buy_cat(cat)
     new_cat = Cat.new(cat)
-    self.pets[:cats] << new_cat.name
+    self.pets[:cats] << new_cat
   end
 
   def buy_dog(dog)
     new_dog = Dog.new(dog)
-    self.pets[:dogs] << new_dog.name
+    self.pets[:dogs] << new_dog
   end
 
   def walk_dogs
